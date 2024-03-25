cd785edfe694b29ea3873f69588e8a15b3e44074
NicolaTeora <nikxodia@gmail.com>
2024-03-25 17:23:19 +0100

    feat : start frist migration

---
+++ database/migrations/2024_03_25_160757_create_comics_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 100);
+            $table->string('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price', 10);
+            $table->string('series', 25);
+            $table->date('sale_date');
+            $table->string('type', 15);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
