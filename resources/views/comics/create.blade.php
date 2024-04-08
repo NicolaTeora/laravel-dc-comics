43461872cd4635edc59da7f0f1b2e95ff93f2759
NicolaTeora <nikxodia@gmail.com>
2024-04-08 11:39:55 +0200

    feat : dev create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,12 +7,32 @@
 
         <div class="container py-4">
             <h2>Add new Comic</h2>
-            <form>
+            <form action="" method="POST">
                 @csrf
-                <div class="mb-3  col-3">
-                    <label for="title" class="form-label">Title:</label>
-                    <input type="text" name="title" class="form-control" id="title" aria-describedby="emailHelp">
+                <div class="row">
+
+                    <div class="mb-3  col-3">
+                        <label for="title" class="form-label">Title:</label>
+                        <input type="text" name="title" class="form-control" id="title">
+                    </div>
+
+                    <div class="mb-3  col-3">
+                        <label for="series" class="form-label">Series:</label>
+                        <input type="text" name="series" class="form-control" id="series">
+                    </div>
+
+                    <div class="mb-3  col-3">
+                        <label for="price" class="form-label">Price:</label>
+                        <input type="number" min=0 step=0.01 name="price" class="form-control" id="price">
+                    </div>
+
+                    <div class="col-12">
+                        <label for="description" class="form-label">Description:</label>
+                        <textarea name="description" class="form-control" id="" cols="30" rows="10"></textarea>
+                    </div>
+
                 </div>
+
             </form>
     </section>
 @endsection
